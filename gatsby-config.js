10205e84c168829ed86b30e8e829b39b59ce5513
gknook <gknook@gmail.com>
2019-07-14 15:51:05 +0200

    tailwind + gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,5 +30,8 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+
+    // Plugins I added
+    `gatsby-plugin-postcss`, // To make tailwind work.
   ],
 }
--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,15 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import Image from "../components/image"
+
 const Header = ({ siteTitle }) => (
-  <header
+  <header class="bg-cover mb-8" 
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      backgroundImage: `url(../images/IMG_1923_bg.png)`
     }}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
+    
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -27,7 +22,7 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-    </div>
+  
   </header>
 )
 
